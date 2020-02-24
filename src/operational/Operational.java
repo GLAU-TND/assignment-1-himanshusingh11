8e6adffe4c2eb81ad6490151559682c56a44a3af
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-24 21:33:18 +0530

    added run method

--- src/operational/Operational.java
+++ src/operational/Operational.java
@@ -1,5 +1,26 @@
 package operational;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Operational {
+    public int run(){
+        System.out.println("Welcome Your Contact List App\n" +
+                "1.add a new contact\n" +
+                "2.view all contacts\n" +
+                "3.search for a contact\n" +
+                "4.delete a contact\n" +
+                "5.exit\n");
+        System.out.print("INPUT : ");
+        Scanner scan =new Scanner(System.in);
+        int input;
+        try{
+            input=scan.nextInt();
+            return input;
+        }
+        catch (InputMismatchException e){
+            return -1;
+        }
+    }
 
 }
