ef76100b9d8f63ad9db64488a55f3779e9408348
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-24 21:41:06 +0530

    added insert method

--- src/storage/LinkedList.java
+++ src/storage/LinkedList.java
@@ -1,21 +1,24 @@
 package storage;
 
+import adt.People;
+
 public class LinkedList {
-   /* Node head;
-    public void insert(People data){
-        Node.size=Node.size+1;
+    Node head;
+
+    public void insert(People data) {
+        Node.size = Node.size + 1;
         Node node = new Node();
-        node.data=data;
-        node.next=null;
-        if(head==null){
-            head=node;
-        }
-        else{
-            Node n=head;
-            while(n.next!=null){
-                n=n.next;
+        node.data = data;
+        node.next = null;
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
             }
-            n.next=node;
+            n.next = node;
         }
-    }*/
+    }
+
 }
