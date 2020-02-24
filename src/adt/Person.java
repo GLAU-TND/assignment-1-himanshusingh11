48c70e472a1ba00e8250ad5dfc318443bb5c4be6
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-24 21:29:48 +0530

    added getter and setter for the Person class

---
+++ src/adt/Person.java
@@ -0,0 +1,47 @@
+package adt;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String number;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setMultiNumber(String number) {
+        this.number = this.number+","+number;
+    }
+
+    public void setMultiEmail(String email) {
+        this.email = this.email+","+email;
+    }
+}
