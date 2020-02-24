37ce5eeba6a09afbf0a64c425c96788fe51ab20f
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-24 22:03:17 +0530

    added main class

--- src/validate/Validation.java
+++ src/validate/Validation.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class Validation {
     final static String EMAIL = "^([a-zA-Z0-9_\\.]+)@([a-zA-Z0-9_\\.]+)\\.([a-zA-Z]{2,5})$";
-
+    final static String MOBILE = "^[0][1-9]\\d{9}$|^[1-9]\\d{9}$";
     public static String emailVal(String input) {
         String output = "";
         Pattern pattern = Pattern.compile(EMAIL);
@@ -23,5 +23,23 @@ public class Validation {
         }
         return output;
     }
+
+    public static String numberVal(String input) {
+        String output = "";
+        Pattern pattern = Pattern.compile(MOBILE);
+        Matcher m = pattern.matcher(input);
+        boolean result = m.matches();
+        if (result) {
+            output = input;
+        } else {
+            System.out.println("Invalid Contact Number!\nRetry : ");
+            Scanner scan = new Scanner(System.in);
+            String newMobile = scan.next();
+            String newResult = numberVal(newMobile);
+            output = newResult;
+        }
+        return output;
+    }
+
 
 }
