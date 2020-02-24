4b5c3d5fd510b267ab57e6744905c2abbae1f0f9
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-24 21:55:17 +0530

    added search() method

--- src/storage/LinkedList.java
+++ src/storage/LinkedList.java
@@ -1,6 +1,7 @@
 package storage;
 
 import adt.People;
+import operational.Operational;
 
 import java.util.Arrays;
 
@@ -67,6 +68,29 @@ public class LinkedList {
         }
         Arrays.sort(list);
         return list;
+    }
+
+    public void search(String ele) {
+        Node n = head;
+        if (n == null) {
+            System.out.println("Empty ContactList!");
+        }
+        int count = 0;
+        while (n != null) {
+            if (n.data.getFirstName().equals(ele)) {
+                count = count + 1;
+                System.out.println("-------- * -------- * -------- * --------");
+
+                System.out.println("First Name : " + n.data.getFirstName() + "\nLast Name : "
+                        + n.data.getLastName());
+                Operational.formattedEmailContact(n.data.getNumber() + "!" + n.data.getEmail());
+                System.out.println("-------- * -------- * -------- * --------");
+                n = n.next;
+            } else {
+                n = n.next;
+            }
+        }
+        System.out.println(count + " match(s) found!\n\n");
     }
 
 
